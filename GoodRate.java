8617f014e3dd34fccbe64d7f6dd75a371f5814f7
MINAMINO <causewaybar711@gmail.com>
2023-09-22 14:55:23 +0900

    Create GoodRate.java

---
+++ GoodRate.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int GoodRate = 40;
+
+        if (GoodRate >= 90){
+            System.out.println("papapa");
+        }
+        else if (GoodRate >= 60 && GoodRate <= 90){
+            System.out.println("you are a good man!");
+        }
+        else {
+            System.out.println("see you!");
+        }
+
+
+    }
+}
+
