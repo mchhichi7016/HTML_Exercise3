025b42987ecf4733f0c754503b9ad9c05e28577c
MINAMINO <causewaybar711@gmail.com>
2023-09-22 14:11:04 +0900

    Create GetOffWork.java

---
+++ GetOffWork.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        boolean get_off_work;
+        boolean watermelon_seller;
+
+
+
+        if (get_off_work = true){
+            System.out.println("buy a suica!");
+            if (watermelon_seller = true){
+                System.out.println("buy one");
+            }
+        }
+
+    }
+}
+
