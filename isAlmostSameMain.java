324074e97991ccc4a9be1d79d6ce49a9a4df2ea5
MINAMINO <causewaybar711@gmail.com>
2023-09-23 15:02:41 +0900

    Create isAlmostSameMain.java

---
+++ isAlmostSameMain.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+
+
+
+        String word1 = "aaaa";
+        String word2 = "bccb";
+
+        boolean result = isAlmostSame.isAlmostSame(word1, word2);
+
+        System.out.println(result); // 应该输出 false
+
+    }
+}
