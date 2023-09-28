6e023ddb053a8b65cdbdc22483203ae000668371
MINAMINO <causewaybar711@gmail.com>
2023-09-28 11:33:55 +0900

    Create oddNumber.java

---
+++ oddNumber.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args){
+
+        int n = 10;
+        int i = 0;
+        int res;
+
+        while (i <= n){
+            if (i % 2 != 0){
+                res = i;
+                System.out.println(res);
+            }
+            i++;
+        }
+
+
+    }
+}
